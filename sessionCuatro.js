f45c5c47372f748b310388f4968a082fa9669865
DanielaCalisaya <hdaniela127c@gmail.com>
2022-07-18 20:27:48 -0500

    feat(sessionCuatro):metodos mas utilizados

--- sessionCuatro.js
+++ sessionCuatro.js
@@ -30,3 +30,25 @@ console.log(plantilla); // En consola veremos el html
 // Introducción de variables en HTML 
 let libros = ["El corredor del laberinto", "La fábrica de chocolate"]
 console.log(libros);
+
+/* MÉTODOS MÁS UTILIZADOS CON CADENAS DE CARÁCTERES */
+
+// Cómo obtener la longitud de un string
+let string = "Soy un string"
+console.log(string.length); //Me indica 13, la cantidad de carácteres
+
+//Obtener partes de cadenas de caracteres
+// slice() substring() substr()
+let Slice = string.slice(0, 8)
+console.log(Slice); //Me traerá de la posición 0 a la 8, el rango que le doy
+let Substring = string.substring(5, 10)
+console.log(Substring); //No hay mucha diferencia con la anterior
+let Substr = string.substr(2, 8)
+console.log(Substr); //Nos traerá una cadena de 10 caracteres pero empezando desde la posición 5
+
+// Reemplazar parte del contenido de una cadena de texto
+let cadena = "soy una cadena"; 
+console.log(cadena.replace("cadena", "persona")); // Lo que quiero sacar, lo que quiero agregar
+let textoLargo = "lorem lorem lorem lorem lo rem lo remlorem"
+console.log(textoLargo.replace("lorem", "hola")); // Me reemplaza solo el primer lorem|la primer instancia
+console.log(textoLargo.replace(/lorem/g, "hola")); // Me reemplaza todas las palabras similares
