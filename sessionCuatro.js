d5961fdc6488e7f8cb292d2414a799a0903bbcfd
DanielaCalisaya <hdaniela127c@gmail.com>
2022-07-18 23:25:22 -0500

    feat(sessionCuatro):metodos de strings II

--- sessionCuatro.js
+++ sessionCuatro.js
@@ -52,3 +52,42 @@ console.log(cadena.replace("cadena", "persona")); // Lo que quiero sacar, lo que
 let textoLargo = "lorem lorem lorem lorem lo rem lo remlorem"
 console.log(textoLargo.replace("lorem", "hola")); // Me reemplaza solo el primer lorem|la primer instancia
 console.log(textoLargo.replace(/lorem/g, "hola")); // Me reemplaza todas las palabras similares
+
+
+/* MANIPULACIÓN DE CADENAS DE TEXTO II*/
+let input = "Enero"
+let db = "enero"
+console.log(input === db); //En el input la persona puede escribir cualquier cosa
+
+// toLowerCase() - toUpperCase()
+console.log(input.toLowerCase() === db.toLowerCase()); //No importa que se escriba, dará true
+console.log(input.toLowerCase()) //minusculas
+console.log(input.toUpperCase()) //mayusculas
+
+console.log(input.toLocaleLowerCase()) //es lo mismo
+
+//Formas de concatenar dos cadenas de carácteres
+let cadenaUno = "Soy la primer cadena.";
+let cadenaDos = " Soy la segunda cadena";
+console.log(cadenaUno.concat(cadenaDos)); //Primer forma
+console.log(cadenaUno + " " + cadenaDos); //Segunda forma
+console.log(`${cadenaUno} ${cadenaDos}`); //Tercer forma
+
+//Eliminar espacios al inicio y al final
+let espacio = "    Soy un string con espacios al final     ";
+console.log(espacio.length); //Me dirá el total de caracteres (44)
+//trim()
+console.log(espacio.trim().length); //Me sacará los espacios al principio y al final (35)
+console.log(espacio.trimStart().length); //Saca solo los espacios del principio
+console.log(espacio.trimEnd().length); //Saca los espacios del final
+
+// Obtener el caracter que hay en cierta posición
+let numero = "Soy el string numero 4"
+console.log(numero.charAt(2)); //Le indicamos que posicion queremos ver, imprimirá la letra de esa posición
+console.log(numero[2]); //Lo mismo que en el caso anterior, me devuelve la letra
+
+// Obtener una palabra dentro de una cadena de carácteres
+let caracteres = "Hello, mi name is Daniela";
+console.log(caracteres.indexOf("Daniela")); // Nos devuelve la posición de la primera letra de la palabra que buscamos, si no lo encuentra nos devuelve -1
+console.log(caracteres.charAt(1)); 
+console.log(caracteres.lastIndexOf("Daniela")); // Nos pasa la ultima posición si es que esa palabra se repitiera
