b01c19a47acc83b8b0801d8a694e3fbc0af1b314
DanielaCalisaya <hdaniela127c@gmail.com>
2022-07-19 21:02:02 -0500

    feat(sessionCinco):operaciones y redondeo

--- sessionCinco.js
+++ sessionCinco.js
@@ -18,3 +18,46 @@ En realidad debe dar 0.3 */
 //QUÉ SE PODRIA HACER? una opción -> 
 //Redondear hacia abajo sin decimales, asi obtenemos un valor más real
 console.log(Math.round((0.1 + 0.2) * 100) / 100); //también hay librerias que podrian ayudar en esto
+
+console.log("----------------------");
+/* OPERACIONES Y REDONDEO */
+//Principales operaciones aritméticas
+let d = 3.5;
+let e = 4.8;
+//Suma
+console.log(d + e);
+//Resta
+console.log(d - e);
+//Multiplicación
+console.log(d * e);
+//División
+console.log(d / e);
+
+//Representación de los números en cadenas de texto
+console.log(typeof d); //Me indica que 'd' es un number
+let string = d.toString()
+
+console.log(string);
+console.log(typeof string); //Me indica que es un string
+
+//Redondeo de números decimales
+let f = 3.3;
+let g = e * 3;
+console.log(g);
+console.log(typeof g); //number
+
+//toFixed() - Limita el numero de decimales al valor x
+console.log(g.toFixed(5)); 
+//Dentro de los parentesis le indico cuantos numeros decimales quiero tener
+console.log(typeof g.toFixed(5)); //string
+//Pero nos está convirtiendo en string lo que le pasamos
+
+//toPrecision(x) - redondeo cientifico - limita el numero de cifras significativas
+let h = 1552.222222255
+let j = 16266256265959536
+console.log(h.toPrecision(6)); 
+//Le paso por parentesis solo la cantidad de numeros que quiero que me traiga
+console.log(j.toPrecision(6));
+/* En consola me da 1.62663e+16. El e+12 quiere decir
+que es multiplicado por 10 y elevado a 12 */
+console.log( typeof j.toPrecision(6)); //devuelve un string
