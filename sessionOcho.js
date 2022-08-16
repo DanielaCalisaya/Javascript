af3717e230e3a10400c6415c85466e86ccf3e46c
DanielaCalisaya <hdaniela127c@gmail.com>
2022-08-16 18:50:41 -0500

    feat(sessionOcho)arrow functions

--- sessionOcho.js
+++ sessionOcho.js
@@ -47,3 +47,33 @@ function multiplicar(a = 0, b = 0) {
 en cambio si hago una variable dentro, desde afuera no puedo acceder a ella,
 dirá que es undefined */
 console.log(multiplicar(4, 9));
+console.log("---------------------");
+
+
+/* Funciones de tipo flecha y anónimas(a partir del 2015 se introdujeron) */
+const array = [1, 2, 4, 6, 10, 14];  
+/* 
+const arrayDos = array.map(function(valor) {
+    return valor * 2
+})
+Seria mejor si usamos las funciones de tipo flecha 
+*/
+const arrayDos = array.map((valor) => valor * 2) // -> Función anónima porque no la estamos asignando a ningún nombre
+console.log(arrayDos); //Obtengo el doble
+
+/* Para definir una función de tipo flecha se la puede 
+asignar a una variable, tipo var, let o const */
+
+/* const dobleDelValor = valor => {
+    return valor * 2
+}    Es lo mismo */
+
+const dobleDelValor = valor => valor * 2
+console.log(dobleDelValor(6)); 
+//Da bien el resultado en la función anterior pero si pongo el console antes de la función me da error porque me dice que no se ha inicializado(hoisting)
+//Estas funciones flecha se puede acceder a ellas, después de que se hayan inicializado
+
+function doble(valor) {
+    return valor * 2
+}
+console.log(doble(4)); //Este console si lo pongo aca o antes de la función tradicional no me dará error
