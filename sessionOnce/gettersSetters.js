7483b716fe1a06757d05eab19a4bae81da50cb4e
DanielaCalisaya <hdaniela127c@gmail.com>
2022-09-12 23:34:17 -0500

    feat(sessionOnce):getters y setters

---
+++ sessionOnce/gettersSetters.js
@@ -0,0 +1,19 @@
+/* 
+getter : métodos que nos permiten obtener atributos/métodos 
+privados o protegidos 
+setter : métodos que nos permiten cambiar el valor de alguno
+de los atributos privados o protegidos 
+*/
+
+/* obtenerNombre(){   
+    return this.#nombre
+}          -> funcion getter: nos permite acceder de forma 
+                     controlada a algun atributo protegido
+*/
+
+/* setEdad(nuevaedad) {       -> setter
+    this.#edad = nuevaedad
+} 
+persona.setEdad(34)
+console.log(persona.getEdad())
+*/
