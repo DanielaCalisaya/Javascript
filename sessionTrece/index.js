06c5a3647ca4f38c33da38ca862ea457615355e7
DanielaCalisaya <hdaniela127c@gmail.com>
2022-09-19 23:13:31 -0500

    feat(sessionTrece):que es linting y ESLint

---
+++ sessionTrece/index.js
@@ -0,0 +1,7 @@
+/* ESlint 
+-> Encuentra y arregla problemas en el código Javascript, en tiempo real
+link - https://eslint.org/
+
+Se pueden configurar de tal modo que nos ayude a homogeneizar el código, 
+por ejemplo el tema de las comillas, los espacios, las tabulaciones, etc.
+*/
