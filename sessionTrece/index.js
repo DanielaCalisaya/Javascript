5564730028395d2c28a0bae81e96755edcf83b14
DanielaCalisaya <hdaniela127c@gmail.com>
2022-09-19 23:57:25 -0500

    feat(sessionTrece):configuracion ESLint

--- sessionTrece/index.js
+++ sessionTrece/index.js
@@ -5,3 +5,18 @@ link - https://eslint.org/
 Se pueden configurar de tal modo que nos ayude a homogeneizar el código, 
 por ejemplo el tema de las comillas, los espacios, las tabulaciones, etc.
 */
+
+const nombre = "Daniela"
+    var nombreDos = 'Chingu';
+    /* en teoria está bien, pero tiene ciertas inconsistencias
+    (comillas dobles y simples, const y var, espacios) */
+
+/* En este caso me diria que hay errores al posicionarnos sobre la palabra */
+
+var objeto = {
+    nombre: 'Circulo',
+    radio: 2,
+    nombre: "Rectángulo"
+}
+
+/* Otra extención que puede ser útil, es Error Lens, marca el error en la misma línea */
