afba8d435d6ae5bf9b35cb6bd5a3c9d2a703142a
DanielaCalisaya <hdaniela127c@gmail.com>
2022-09-21 23:00:58 -0500

    feat(sessionTrece):instalacion y configuracion de ficheros de eslint

---
+++ sessionTrece/eslint-npm/index.js
@@ -0,0 +1,14 @@
+/* La anterior manera de usarlo era manual, de muestra.
+Lo mejor seria usar esta manera:
+Al ingresar a la página, vamos a get started
+
+npm init
+npm init @eslint/config 
+
+Me hará varias preguntas sobre el proyecto o el entorno que estoy usando, y qué correcciones quiero en mi proyecto
+
+En el package.json al final, me dice la dependencia instalada y su version */
+
+const nombre = "Daniela" 
+/* Override: "no-unused-vars": "off" colocamos lo anterior en el json, sobreescribimos, pero es de forma permanente en todo el proyecto, no solo en esa línea */
+const persona2 = 'Maria' 
