c6d88beffb956cbf365ec1bf8e7c98fe5ebebe8f
DanielaCalisaya <hdaniela127c@gmail.com>
2022-12-16 23:49:35 -0300

    Geolocalización

--- sessionDiesisiete/index.js
+++ sessionDiesisiete/index.js
@@ -6,8 +6,10 @@ function initMap() {
    //console.log("Hello maps!");
    //definir una posicion
    const posicion = { 
-        lat: -25.363,
-        lng: 131.044
+        /* lat: -25.363,   -> australia
+        lng: 131.044 */
+        lat: -38.958,
+        lng: -68.0702
     }
 
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -23,4 +25,43 @@ function initMap() {
     map,
     title: "Posicion inicial"
     })
+
+    //Obtener la geolocalizacion y desp hacer un setPosition
+    //market.setPosition({ lat, lng })
+    geoPosiciona()
 }
+
+//Si queremos que el mapa se posicione en nuestra ubicación
+
+function geoPosiciona() {  
+    if (navigator.geolocation) {
+       const geoLoc = navigator.geolocation 
+       const posicion = geoLoc.watchPosition()
+    } else {
+        alert("Tu navegador no admite geolocalización")
+        //Quizas hayan navegadores muy antiguos que no soporten
+    }
+}
+
+function centraMapa(position) {
+    const nuevaPos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    }
+    market.setPosition(nuevaPos)
+    map.setCenter(nuevaPos)
+}
+
+function onError(error) {
+    console.log("Se produjo un error");
+    console.error(error);
+}
+
+/* usando la consola del navegador se puede acceder a la geolocalizacion
+      -> asi accedemos a la propiedad de este navegador y watchPosition acepta 3 metodos
+    navigator.geolocation.watchPosition(position => {
+        console.log(position)
+    })
+
+    Me devuelve un objeto GeolocationPosition
+*/
