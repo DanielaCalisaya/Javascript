5f39b0d5a76efe492d91f2e7e1823e62af60208c
DanielaCalisaya <hdaniela127c@gmail.com>
2022-12-21 23:03:12 -0300

    marcado html

--- entregableSessionDiecisiete/index.js
+++ entregableSessionDiecisiete/index.js
@@ -2,3 +2,10 @@
 - Crea un mapa en HTML
 - Pon chinchetas en tus 3 lugares favoritos del planeta tierra 
 */
+
+//Lugares favoritos
+/* -Cataratas de Iguazú - Misiones - Argentina
+   -Nueva York - Estados Unidos
+   -Río de Janeiro - Brasil
+*/
+
